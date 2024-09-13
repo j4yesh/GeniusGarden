b308d72b7a2655fa99e25d8880395f70c2a9a6bf
j4yesh <jayeshbadgujar7052@gmail.com>
2024-09-13 08:25:58 +0530

    knock

---
+++ Assets/Scripts/Loading.cs
@@ -0,0 +1,29 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI; 
+public class Loading : MonoBehaviour
+{  
+    private Animation anim;
+    private Image image;
+
+
+    void Start()
+    {
+         anim = GetComponent<Animation>();
+         image = GetComponent<Image>();
+         image.enabled=false;
+    }
+
+    public void show(){
+        anim.enabled=true;
+        image.enabled=true;
+        anim.Play("circle_half_rotating_fading_6_1");
+    }
+
+    public void hide(){
+        anim.Stop("circle_half_rotating_fading_6_1");
+        image.enabled=false;
+        anim.enabled=false;
+    }
+}
